1c96c24d899d7f059d55d061f8cd36317f84078d
ronttizz <toni.nissinen@gmail.com>
2022-03-28 14:38:29 +0300

    Removed unused components and used different stucture in main to work with class component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-import Button from "./Button";
-import Counter from "./Counter";
+// import Button from "./Button";
+// import Counter from "./Counter";
 
-const Main = () => {
-  return (
-    <main className="main">
-      <Counter count="0" />
-      <div className="buttons">
-        <Button text="Add five" />
-        <Button text="Add one" />
-        <Button text="Reset" />
-        <Button text="Remove one" />
-        <Button text="Remove five" />
-      </div>
-    </main>
-  );
+class Main extends Component {
+  state = {
+    counter: 0,
+  };
+
+  render() {
+    return (
+      <main className="main">
+        <div className="counter">
+          <h1 className="count">{this.state.counter}</h1>
+        </div>
+        <div className="buttons">
+          <button onClick={clickHandler}>Add five</button>
+          <button onClick={clickHandler}>Add one</button>
+          <button onClick={clickHandler}>Reset</button>
+          <button onClick={clickHandler}>Remove one</button>
+          <button onClick={clickHandler}>Remove five</button>
+        </div>
+      </main>
+    );
+  }
+}
+
+const clickHandler = () => {
+  console.log("clicked");
 };
 
 export default Main;
