f2ad6a2a61a60c36bf28e7ebd501709fa2446272
ronttizz <toni.nissinen@gmail.com>
2022-03-28 14:10:02 +0300

    Removed template logo from manifest

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Button = (props) => {
-  return (
-    <button className="button" onClick={props.function}>
-      {props.text}
-    </button>
-  );
+  return <button className="button">{props.text}</button>;
 };
 
 export default Button;
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,11 +8,11 @@ const Main = () => {
     <main className="main">
       <Counter count="0" />
       <div className="buttons">
-        <Button text="Add five" function="addFive" />
-        <Button text="Add one" function="addOne" />
-        <Button text="Reset" function="reset" />
-        <Button text="Remove one" function="removeOne" />
-        <Button text="Remove five" function="removeFive" />
+        <Button text="Add five" />
+        <Button text="Add one" />
+        <Button text="Reset" />
+        <Button text="Remove one" />
+        <Button text="Remove five" />
       </div>
     </main>
   );
